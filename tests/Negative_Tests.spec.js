608df67a0abc3d0b43a19c1517149feaa9e3cf3d
Mashruf Rohon <mashrufrohon@gmail.com>
2025-12-23 18:17:01 +0600

    added comments for negative tests

--- tests/Negative_Tests.spec.js
+++ tests/Negative_Tests.spec.js
@@ -1,70 +1,85 @@
 const { test, expect } = require('@playwright/test');
 
 test("TC_NEG_001: Verify 404 for invalid endpoint", async ({ request }) => {
+
+    // sending request and storing the response
     const invalidEndpoint = await request.get(`/postss`);
 
-    expect(invalidEndpoint.status()).toBe(404);
+    expect(invalidEndpoint.status()).toBe(404); // asserting the status code
 })
 
 test("TC_NEG_002: Verify single post is not retrieved with invalid id", async ({ request }) => {
 
+    // setting invalid id
     const id = 999;
+
+    // sending request and storing the response
     const invalidId = await request.get(`/posts/${id}`);
 
-    expect(invalidId.status()).toBe(404);
+    expect(invalidId.status()).toBe(404); // asserting the status code
 })
 
 test("TC_NEG_003: Verify single post is not retrieved with invalid endpoint", async ({ request }) => {
 
-    const id = 10;
+    const id = 10; // setting id between 1-100
+
+    // sending request and storing the response
     const invalidEndpoint = await request.get(`/postss/${id}`);
 
-    expect(invalidEndpoint.status()).toBe(404);
+    expect(invalidEndpoint.status()).toBe(404); // asserting the status code
 })
 
 test("TC_NEG_004: Verify all comments of a post is not retrieved with invalid endpoint", async ({ request }) => {
 
-    const id = 10;
+    const id = 10; // setting id
+
+    //  sending request and storing response
     const invalidEndpoint = await request.get(`/postss/${id}/comments`);
 
-    expect(invalidEndpoint.status()).toBe(404);
+    expect(invalidEndpoint.status()).toBe(404); // asserting status code
 })
 
 test("TC_NEG_005: Verify all comments of a post is not retrieved with invalid id", async ({ request }) => {
 
-    const id = 999;
+    const id = 999; // setting invalid id
+
+    // sending request and storing the response
     const invalidId = await request.get(`/posts/${id}/comments`);
 
-    expect(invalidId.status()).toBe(404);
+    expect(invalidId.status()).toBe(404); // asserting status code
 })
 
 test("TC_NEG_006: Verify all comments of a post are not retrieved by query parameter with invalid endpoint", async ({ request }) => {
 
-    const id = 10;
+    const id = 10; // setting id between 1-100
+
+    // sending request and storing the response
     const invalidEndpoint = await request.get(`/commentss`, {
         params: {
             id: id
         }
     });
 
-    expect(invalidEndpoint.status()).toBe(404);
+    expect(invalidEndpoint.status()).toBe(404); // asserting the status code
 })
 
 test("TC_NEG_007: Verify all comments of a post are not retrieved by query parameter with invalid id", async ({ request }) => {
 
-    const invalidId = 999;
+    const invalidId = 999; // setting invalid id
+
+    // sending request and storing response
     const invalId = await request.get(`/comments`, {
         params: {
             id: invalidId
         }
     });
 
-    expect(invalId.status()).toBe(404);
+    expect(invalId.status()).toBe(404); // assertion the status code
 })
 
 test("TC_NEG_008: Verify creating a post with empty request body", async ({ request }) => {
 
-
+    // sending request and storing the response
     const post = await request.post(`/posts`, {
         data: {
 
@@ -75,17 +90,20 @@ test("TC_NEG_008: Verify creating a post with empty request body", async ({ requ
     })
 
 
-    expect(post.status()).toBe(400);
+    expect(post.status()).toBe(400); // asserting the status code
 })
 
 test("TC_NEG_009: Verify creating a post with invalid data type", async ({ request }) => {
 
-    const userId = "1";
+    const userId = "1"; // setting invalid type of user id
+    const title = "foo"; // setting title
+    const body = "bar"; // setting body
 
+    // sending request and storing the response
     const post = await request.post(`/posts`, {
         data: {
-            tittle: "foo",
-            body: "bar",
+            tittle: title,
+            body: body,
             userId: userId
         },
         headers: {
@@ -93,16 +111,17 @@ test("TC_NEG_009: Verify creating a post with invalid data type", async ({ reque
         }
     })
 
-    expect(post.status()).toBe(400);
+    expect(post.status()).toBe(400); // asserting the status code
 })
 
 test("TC_NEG_010: Verify full update of a post with invalid id", async ({ request }) => {
 
-    const title = "foo";
-    const body = "lol";
-    const userId = 1;
-    const id = 999;
+    const title = "foo"; // setting title
+    const body = "lol"; // setting body
+    const userId = 1; // setting user id between 1-10
+    const id = 999; //  setting invalid id
 
+    // sending request and storing the response
     const updateFullPost = await request.put(`/posts/${id}`, {
         data: {
             id: id,
@@ -114,15 +133,16 @@ test("TC_NEG_010: Verify full update of a post with invalid id", async ({ reques
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    expect(updateFullPost.status()).toBe(404);
+    expect(updateFullPost.status()).toBe(404); // asserting status code
 
 })
 
 test("TC_POS_011: Verify full update of a post with missing required fields", async ({ request }) => {
 
-    const userId = 1;
-    const id = 10;
+    const userId = 1; // setting id between 1-10
+    const id = 10; // setting user id between 1-100
 
+    // sending request and storing the response
     const updateFullPost = await request.put(`/posts/${id}`, {
         data: {
             userId: userId
@@ -131,17 +151,18 @@ test("TC_POS_011: Verify full update of a post with missing required fields", as
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    expect(updateFullPost.status()).toBe(400);
+    expect(updateFullPost.status()).toBe(400); // asserting status code
 
 })
 
 test("TC_NEG_012: Verify full update of a post with invalid data type", async ({ request }) => {
 
-    const title = "foo";
-    const body = "lol";
-    const userId = "1";
-    const id = 10;
+    const title = "foo"; // setting title
+    const body = "lol"; // setting body
+    const userId = "1"; // setting invalid type of user id
+    const id = 10; // setting id between 1-100
 
+    // sending request and storing the response
     const updateFullPost = await request.put(`/posts/${id}`, {
         data: {
             id: id,
@@ -153,31 +174,33 @@ test("TC_NEG_012: Verify full update of a post with invalid data type", async ({
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    expect(updateFullPost.status()).toBe(400);
+    expect(updateFullPost.status()).toBe(400); // asserting the status code
 
 })
 
 test("TC_NEG_013: Verify full update of a post with empty request body", async ({ request }) => {
 
-    const id = 10;
+    const id = 10; // setting id between 1-100
 
+    // sending request and storing the response
     const updateFullPost = await request.put(`/posts/${id}`, {
         data: {},
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    expect(updateFullPost.status()).toBe(400);
+    expect(updateFullPost.status()).toBe(400); // asserting the status code
 
 })
 
 test("TC_NEG_014: Verify full update of a post with invalid endpoint", async ({ request }) => {
 
-    const title = "foo";
-    const body = "lol";
-    const id = 10;
-    const userId = 1;
+    const title = "foo"; // setting title
+    const body = "lol"; // setting body
+    const id = 10; // setting id between 1-100
+    const userId = 1; // setting user id between 1-10
 
+    // sending request and storing the response
     const updateFullPost = await request.put(`/postss/${id}`, {
         data: {
             id: id,
@@ -189,15 +212,16 @@ test("TC_NEG_014: Verify full update of a post with invalid endpoint", async ({ 
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    expect(updateFullPost.status()).toBe(404);
+    expect(updateFullPost.status()).toBe(404); // asserting the status code
 
 })
 
 test("TC_NEG_015: Verify partial update of a post with invalid id", async ({ request }) => {
 
-    const id = 999;
-    const body = "Fruits"
+    const id = 999; // setting invalid id
+    const body = "Fruits" // setting body
 
+    // sending request and storing the response
     const updatePartialPost = await request.patch(`/posts/${id}`, {
         data: {
             title: body
@@ -206,15 +230,16 @@ test("TC_NEG_015: Verify partial update of a post with invalid id", async ({ req
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    expect(updatePartialPost.status()).toBe(404);
+    expect(updatePartialPost.status()).toBe(404); // asserting the status code
 
 })
 
 test("TC_NEG_016: Verify partial update of a post with invalid data type", async ({ request }) => {
 
-    const id = 10;
-    const body = 1000;
+    const id = 10; // setting id between 1-100
+    const body = 1000; // setting invalid data type for body
 
+    // sending request and storing the response
     const updatePartialPost = await request.patch(`/posts/${id}`, {
         data: {
             body: body
@@ -223,29 +248,31 @@ test("TC_NEG_016: Verify partial update of a post with invalid data type", async
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    expect(updatePartialPost.status()).toBe(400);
+    expect(updatePartialPost.status()).toBe(400); // asserting the status code
 
 })
 
 test("TC_NEG_017: Verify partial update of a post with empty request body", async ({ request }) => {
 
-    const id = 10;
+    const id = 10; // setting the id between 1-100
 
+    // sending request and storing the response
     const updatePartialPost = await request.patch(`/posts/${id}`, {
         data: {},
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    expect(updatePartialPost.status()).toBe(400);
+    expect(updatePartialPost.status()).toBe(400); // asserting the status code
 
 })
 
 test("TC_NEG_018: Verify partial update of a post with invalid endpoint", async ({ request }) => {
 
-    const id = 10;
-    const body = "Fruits"
+    const id = 10; // setting id between 1-100
+    const body = "Fruits" // setting body
 
+    // sending request and storing the response
     const updatePartialPost = await request.patch(`/postss/${id}`, {
         data: {
             body: body
@@ -255,27 +282,29 @@ test("TC_NEG_018: Verify partial update of a post with invalid endpoint", async 
         }
     })
 
-    expect(updatePartialPost.status()).toBe(404);
+    expect(updatePartialPost.status()).toBe(404); // asserting the status code
 
 })
 
 test("TC_NEG_019: Verify deleting a post with invalid endpoint", async ({ request }) => {
 
-    const id = 10;
+    const id = 10; // setting id between 1-100
 
+    // sending request and storing the response
     const deletePost = await request.delete(`/postss/${id}`)
 
-    expect(deletePost.status()).toBe(404);
+    expect(deletePost.status()).toBe(404); // asserting the status code
 
 })
 
 test("TC_NEG_020: Verify deleting a post with invalid id", async ({ request }) => {
 
-    const id = 999;
+    const id = 999; // setting invalid id
 
+    // sending request and storing the response
     const deletePost = await request.delete(`/posts/${id}`)
 
-    expect(deletePost.status()).toBe(404);
+    expect(deletePost.status()).toBe(404); // asserting the status code
 
 })
 
